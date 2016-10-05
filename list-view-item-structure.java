67643966c29aad0d13a34442eb1bd70ce7cd5378
George Lamprakis <george.lamprakis@gmail.com>
2016-10-05 12:38:19 +0100

    Create list-view-item-structure.java

---
+++ list-view-item-structure.java
@@ -0,0 +1,11 @@
+package com.example.mormolis.listviewparsing;
+
+public class Moria {
+
+        String onomaSxolis;
+        int moria;
+        public Moria(String onomaSxolis, int moria){
+            this.onomaSxolis = onomaSxolis;
+            this.moria = moria;
+        }
+}
